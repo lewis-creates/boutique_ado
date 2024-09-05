44903be16f35eb9aad32222e1dc66f8479d6675e
Lewis Cummings <cummingsjr00@gmail.com>
2024-09-05 11:50:39 +0000

    errors from json file

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     # Optional -- requires install using `django-allauth[socialacocunt]`.
     'allauth.socialaccount',
-    'home'
+    'home',
 ]
 
 MIDDLEWARE = [
